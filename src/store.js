4ea9378fbae9a2a3c61319cc098f76633eb2a184
Raposhian <ryan.aposhian@gmail.com>
2020-04-02 15:51:13 -0600

    finished step one

--- src/store.js
+++ src/store.js
@@ -1,1 +1,15 @@
+import {createStore} from 'redux';
 
+initialState = {
+
+}
+
+function reducer (state = initialState, action) {
+   const {type, payload} = action;
+   switch(type) {
+      default:
+         return state;
+   }
+}
+
+export default createStore(reducer);
